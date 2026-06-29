8ba3d7ae8ef66524d95dbc26f978ed01d4387e3d
hhy981422951-debug <hhy981422951@gmail.com>
2026-06-29 16:37:38 +0800

    Refresh offline cache for link flow fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "xianyu-deal-scout-v2";
+const CACHE_NAME = "xianyu-deal-scout-v3";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./assets/icon.svg"];
 
 self.addEventListener("install", (event) => {
